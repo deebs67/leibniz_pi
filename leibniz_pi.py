83f25737f83a18a35f455b9be085b343d2404488
ddnb <damian.bevan@aceaxis.co.uk>
2020-01-23 13:55:54 +0000

    Added new Python file

---
+++ leibniz_pi.py
@@ -0,0 +1,8 @@
+def this_leibniz_term(term_index):
+    return (4.0/(term_index*2+1))*((-1)**(term_index))  
+    
+def leibniz_sum(number_of_terms):
+    term_generator = (this_leibniz_term(x) for x in range(number_of_terms))
+    
+    return sum(term_generator)   
+    
