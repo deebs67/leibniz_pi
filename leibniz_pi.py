33f06d685ae9dfbd0e6285e47304d61b8faf592e
ddnb <damian.bevan@aceaxis.co.uk>
2020-01-29 12:58:23 +0000

    Added function arctan_z_term

--- leibniz_pi.py
+++ leibniz_pi.py
@@ -4,5 +4,9 @@ def this_leibniz_term(term_index):
 def leibniz_sum(number_of_terms):
     term_generator = (this_leibniz_term(x) for x in range(number_of_terms))
     
-    return sum(term_generator)   
+    return sum(term_generator)
+
+def arctan_z_term(z, term_index):
+    return (z/(term_index*2+1))*((-1)**(term_index))      
     
+   
