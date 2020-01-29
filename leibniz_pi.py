1f6faadb41d7d428778a8d6546388c4b79a6022c
ddnb <damian.bevan@aceaxis.co.uk>
2020-01-29 13:25:08 +0000

    First working arctan_z

--- leibniz_pi.py
+++ leibniz_pi.py
@@ -1,12 +1,31 @@
+import math
+
+
 def this_leibniz_term(term_index):
     return (4.0/(term_index*2+1))*((-1)**(term_index))  
     
+
 def leibniz_sum(number_of_terms):
     term_generator = (this_leibniz_term(x) for x in range(number_of_terms))
     
     return sum(term_generator)
 
+
 def arctan_z_term(z, term_index):
-    return (z/(term_index*2+1))*((-1)**(term_index))      
+    """
+    Generates each term in Gregory-Leibniz series for arctan(z)
     
+    """
+    term_power = (term_index*2+1)  # Each term is raised to this power, and also divided by this value
+    
+    return (math.pow(z, term_power)/term_power)*((-1)**(term_index))      
+
+    
+def arctan_z_sum(z, number_of_terms):
+    """
+    Sums over terms in Gregory-Leibniz series for arctan(z)
+    """
+    term_generator = (arctan_z_term(z, x) for x in range(number_of_terms))
+    
+    return sum(term_generator)
    
